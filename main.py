870122be089044a2ed923283927fa1b04b743178
celso.abente16 <celso.abente16@gmail.com>
2023-10-30 15:43:42 -0300

    comit

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+# Get the HTML content of the page
+url = ("https://erpperfect.com/pwebLinkPedido.php?l=P010086129051083126600W")
+response = requests.get(url)
+html_content = response.content
+# Parse the HTML content
+soup = BeautifulSoup(html_content, "html.parser")
+# Find all the images on the page
+images = soup.find_all("img")
+# Create a directory to store the images
+if not os.path.exists("images"):
+    os.mkdir("images")
+# Download the images
+for image in images:
+    image_url = image.get("src")
+    if image_url.startswith("http://") or image_url.startswith("https://"):
+        image_name = image_url.split("/")[-1]
+        image_path = os.path.join("images", image_name)
+        response = requests.get(image_url)
+        with open(image_path, "wb") as f:
+            f.write(response.content)
+    else:
+        print(f"Ignorando URL no válida: {image_url}")
