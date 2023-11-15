e3a43acae7e4eb432eb60b4eabdbb4ff849785bd
celso.abente16 <celso.abente16@gmail.com>
2023-11-15 14:11:49 -0300

    cambios a realizar

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from bs4 import BeautifulSoup
 import os
 from urllib.parse import urlparse
 from urllib.parse import unquote
+from PIL import Image
 # Get the HTML content of the page
-url = ("https://www.paramountplasticos.com.br/categoria-produto/organizacao/")
+url = ("https://www.nuevaamericana.com.py/categoria/812/construccionbloque")
 response = requests.get(url)
 html_content = response.content
 # Parse the HTML content
@@ -24,6 +25,14 @@ for image in images:
         response = requests.get(image_url)
         with open(image_path, "wb") as f:
             f.write(response.content)
+            try:
+                with Image.open(image_path) as img:
+                    img = img.convert("RGB")
+                    img.save(image_path.replace(".jpg", ".jpeg"))
+            except Exception as e:
+                print(f"No se pudo convertir la imagen {image_path}: {str(e)}")
+            # Convert to JPEG
     else:
         print(f"Ignorando URL no válida: {image_url}")
+# mostrar un mensaje de porcentaje de descarga
 print("Descarga finalizada")
